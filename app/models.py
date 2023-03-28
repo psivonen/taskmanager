c70e3cdce96991f21290116986c9fdda9c498fb3
psivonen <petra.sivonen@gmail.com>
2023-03-28 14:48:05 +0300

    fake users

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
