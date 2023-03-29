eca99a7cc5036e19e09ffcb1a02bfa313bf9d99c
psivonen <petra.sivonen@gmail.com>
2023-03-29 13:53:22 +0300

    pagination

--- app/main/views.py
+++ app/main/views.py
@@ -15,7 +15,7 @@ def users():
     # users = User.query.order_by(User.username).all()
     page = request.args.get('page', 1, type=int)
     pagination = User.query.order_by(User.username).paginate(
-        page, per_page=current_app.config['SM_ROWS_PER_PAGE'], 
+        page=page, per_page=current_app.config['SM_ROWS_PER_PAGE'], 
         error_out=False)
     users = pagination.items
     return render_template('users.html', users=users, pagination=pagination)
