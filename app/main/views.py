5af6a1522cc9e509b74cacade855cbc437573927
psivonen <petra.sivonen@gmail.com>
2023-03-29 13:14:42 +0300

    Sivutus kesken

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, request, current_app
 from . import main
-from flask_login import login_required
+from flask_login import login_required, current_user
 from ..models import User
 
 
@@ -8,8 +8,14 @@ from ..models import User
 def index():
     return render_template('index.html')
 
+
 @login_required
 @main.route('/users')
 def users():
-    all_users = User.query.order_by(User.username).all()
-    return render_template('users.html', users=all_users)
+    # users = User.query.order_by(User.username).all()
+    page = request.args.get('page', 1, type=int)
+    pagination = User.query.order_by(User.username).paginate(
+        page, per_page=current_app.config['SM_ROWS_PER_PAGE'], 
+        error_out=False)
+    users = pagination.items
+    return render_template('users.html', users=users, pagination=pagination)
